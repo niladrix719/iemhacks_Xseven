4227804991c1fef8c8a5d025ff6ba3283a667459
Niladri <rickyrickado14@gmail.com>
2023-08-26 12:30:55 +0530

    drag functionality added

--- client/app/simulator/page.tsx
+++ client/app/simulator/page.tsx
@@ -1,11 +1,72 @@
-export default function Simulator() {
+'use client'
+import React, { useEffect, useRef } from 'react';
+
+function App() {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const boxRef = useRef<HTMLDivElement>(null);
+  const isClicked = useRef<boolean>(false);
+  const coords = useRef<{
+    startX: number,
+    startY: number,
+    lastX: number,
+    lastY: number
+  }>({
+    startX: 0,
+    startY: 0,
+    lastX: 0,
+    lastY: 0
+  });
+
+  useEffect(() => {
+    if (!boxRef.current || !containerRef.current) return;
+
+    const box = boxRef.current;
+    const container = containerRef.current;
+
+    const onMouseDown = (e: MouseEvent) => {
+      isClicked.current = true;
+      coords.current.startX = e.clientX;
+      coords.current.startY = e.clientY;
+    };
+
+    const onMouseUp = (e: MouseEvent) => {
+      isClicked.current = false;
+      coords.current.lastX = box.offsetLeft;
+      coords.current.lastY = box.offsetTop;
+    };
+
+    const onMouseMove = (e: MouseEvent) => {
+      if (!isClicked.current) return;
+
+      const nextX = e.clientX - coords.current.startX + coords.current.lastX;
+      const nextY = e.clientY - coords.current.startY + coords.current.lastY;
+
+      box.style.top = `${nextY}px`;
+      box.style.left = `${nextX}px`;
+    };
+
+    box.addEventListener('mousedown', onMouseDown);
+    box.addEventListener('mouseup', onMouseUp);
+    container.addEventListener('mousemove', onMouseMove);
+    container.addEventListener('mouseleave', onMouseUp);
+
+    const cleanup = () => {
+      box.removeEventListener('mousedown', onMouseDown);
+      box.removeEventListener('mouseup', onMouseUp);
+      container.removeEventListener('mousemove', onMouseMove);
+      container.removeEventListener('mouseleave', onMouseUp);
+    };
+
+    return cleanup;
+  }, []);
+
   return (
-    <main className="w-screen h-screen">
-      <canvas
-        className="w-full h-full absolute top-0 left-0 bg-white"
-        width={1366}
-        height={605}
-      ></canvas>
+    <main>
+      <div ref={containerRef} className="h-screen w-screen bg-white overflow-hidden relative">
+        <div ref={boxRef} className="h-40 w-40 bg-red-600 absolute cursor-pointer"></div>
+      </div>
     </main>
   );
 }
+
+export default App;
