2699cddc73b1254337e83fd859719680cac117b3
Niladri <rickyrickado14@gmail.com>
2023-08-26 22:02:59 +0530

    free movement added to canvas elements

--- client/app/simulator/page.tsx
+++ client/app/simulator/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import React, { useEffect, useRef, useState } from "react";
 
@@ -6,19 +6,33 @@ function App() {
   const containerRef = useRef<HTMLDivElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const activeBoxRef = useRef<number | null>(null);
-  const prevMousePos = useRef<{ x: number; y: number }>({ x: 0, y: 0 });
+  const activeDotRef = useRef<number | null>(null);
 
   const [shapes, setShapes] = useState<
     {
-      height: number;
-      width: number;
       id: number;
       type: "square" | "circle" | "triangle";
       left: number;
       top: number;
+      offsetX?: number;
+      offsetY?: number;
     }[]
   >([]);
   const shapeIdCounter = useRef<number>(0);
+
+  const [connections, setConnections] = useState<
+    {
+      from: number;
+      to: number;
+    }[]
+  >([]);
+
+  const [draggingLine, setDraggingLine] = useState<{
+    startX: number;
+    startY: number;
+    endX: number;
+    endY: number;
+  } | null>(null);
 
   useEffect(() => {
     if (!containerRef.current || !canvasRef.current) return;
@@ -32,10 +46,13 @@ function App() {
     const context = canvas.getContext("2d");
 
     if (!context) return;
+
     const drawCanvas = () => {
       context.clearRect(0, 0, canvas.width, canvas.height);
 
       context.strokeStyle = "gray";
+
+      const gridSize = 20;
 
       const numXLines = canvas.width / gridSize;
       const numYLines = canvas.height / gridSize;
@@ -55,16 +72,97 @@ function App() {
         context.lineTo(canvas.width, y);
         context.stroke();
       }
+
+      context.strokeStyle = "yellow";
+      context.lineWidth = 2;
+
+      connections.forEach((connection) => {
+        const fromShape = shapes.find((shape) => shape.id === connection.from);
+        const toShape = shapes.find((shape) => shape.id === connection.to);
+
+        if (fromShape && toShape) {
+          context.beginPath();
+          context.moveTo(fromShape.left + 10, fromShape.top + 10);
+          context.lineTo(toShape.left + 10, toShape.top + 10);
+          context.stroke();
+        }
+      });
+
+      if (draggingLine) {
+        context.strokeStyle = "yellow";
+        context.lineWidth = 2;
+        context.beginPath();
+        context.moveTo(draggingLine.startX, draggingLine.startY);
+        context.lineTo(draggingLine.endX, draggingLine.endY);
+        context.stroke();
+      }
     };
 
     drawCanvas();
 
     const onMouseDown = (e: MouseEvent, shapeId: number) => {
+      if (e.target instanceof HTMLElement) {
+        const dotId = parseInt(e.target.dataset.dotId || "", 10);
+        console.log(dotId, "");
+        if (!isNaN(dotId) && dotId === shapeId) {
+          setDraggingLine({
+            startX: e.clientX,
+            startY: e.clientY,
+            endX: e.clientX,
+            endY: e.clientY,
+          });
+          activeDotRef.current = shapeId;
+          return;
+        }
+      }
+
       activeBoxRef.current = shapeId;
-      prevMousePos.current = { x: e.clientX, y: e.clientY };
+      const clickedShape = shapes.find((shape) => shape.id === shapeId);
+      if (clickedShape) {
+        const offsetX = e.clientX - clickedShape.left;
+        const offsetY = e.clientY - clickedShape.top;
+        clickedShape.offsetX = offsetX;
+        clickedShape.offsetY = offsetY;
+      }
     };
 
     const onMouseUp = () => {
+      if (activeBoxRef.current === null && activeDotRef.current === null)
+        return;
+
+      if (draggingLine && activeDotRef.current !== null) {
+        const fromId = shapes.find(
+          (shape) =>
+            shape.left + 10 === draggingLine.startX &&
+            shape.top + 10 === draggingLine.startY
+        )?.id;
+        const toId = activeDotRef.current;
+
+        if (fromId && fromId !== toId) {
+          setConnections([...connections, { from: fromId, to: toId }]);
+        }
+
+        setDraggingLine(null);
+        activeDotRef.current = null;
+        return;
+      }
+
+      const newShapes = shapes.map((shape) => {
+        if (shape.id === activeBoxRef.current) {
+          const gridSize = 20;
+          const left = Math.round(shape.left / gridSize) * gridSize;
+          const top = Math.round(shape.top / gridSize) * gridSize;
+
+          return {
+            ...shape,
+            left,
+            top,
+          };
+        }
+        return shape;
+      });
+
+      setShapes(newShapes);
       activeBoxRef.current = null;
     };
 
@@ -73,35 +171,37 @@ function App() {
 
       const newShapes = shapes.map((shape) => {
         if (shape.id === activeBoxRef.current) {
-          const deltaX = e.clientX - prevMousePos.current.x;
-          const deltaY = e.clientY - prevMousePos.current.y;
-
-          const nextLeft = Math.round((shape.left + deltaX) / gridSize) * gridSize;
-          const nextTop = Math.round((shape.top + deltaY) / gridSize) * gridSize;
-
-          const widthDiff = Math.round(shape.width / gridSize);
-          const heightDiff = Math.round(shape.height / gridSize);
+          const left = e.clientX - shape.offsetX!;
+          const top = e.clientY - shape.offsetY!;
 
           return {
             ...shape,
-            left: nextLeft,
-            top: nextTop,
-            width: widthDiff * gridSize,
-            height: heightDiff * gridSize,
+            left,
+            top,
           };
         }
         return shape;
       });
 
       setShapes(newShapes);
-      prevMousePos.current = { x: e.clientX, y: e.clientY };
+
+      if (draggingLine) {
+        setDraggingLine({
+          startX: draggingLine.startX,
+          startY: draggingLine.startY,
+          endX: e.clientX,
+          endY: e.clientY,
+        });
+      }
     };
 
     container.addEventListener("mousedown", (e) => {
       const clickedShape = e.target as HTMLElement;
       const shapeId = parseInt(clickedShape.dataset.shapeId || "", 10);
-      if (!isNaN(shapeId)) {
-        onMouseDown(e, shapeId);
+      const dotId = parseInt(clickedShape.dataset.dotId || "", 10);
+      if (!isNaN(shapeId) || !isNaN(dotId)) {
+        const clickedShapeId = !isNaN(dotId) ? dotId : shapeId;
+        onMouseDown(e, clickedShapeId);
       }
     });
 
@@ -114,9 +214,7 @@ function App() {
     };
 
     return cleanup;
-  }, [shapes]);
-
-  const gridSize = 40;
+  }, [shapes, connections, draggingLine]);
 
   const addShape = (shapeType: "square" | "circle" | "triangle") => {
     const newShape = {
@@ -124,15 +222,7 @@ function App() {
       type: shapeType,
       left: 0,
       top: 0,
-      width: gridSize,
-      height: gridSize,
     };
-
-    const snapX = Math.round(newShape.left / gridSize) * gridSize;
-    const snapY = Math.round(newShape.top / gridSize) * gridSize;
-
-    newShape.left = snapX;
-    newShape.top = snapY;
 
     setShapes([...shapes, newShape]);
   };
@@ -149,7 +239,7 @@ function App() {
           <div
             key={shape.id}
             data-shape-id={shape.id}
-            className={`h-20 w-20 absolute cursor-pointer ${
+            className={`h-10 w-10 absolute cursor-pointer ${
               shape.type === "circle" ? "rounded-full" : ""
             }`}
             style={{
@@ -157,7 +247,17 @@ function App() {
               left: `${shape.left}px`,
               backgroundColor: "red",
             }}
-          ></div>
+          >
+            <div
+              className="dot-element absolute w-2 h-2 bg-yellow-500 rounded-full cursor-pointer"
+              data-dot-id={shape.id}
+              style={{
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+              }}
+            ></div>
+          </div>
         ))}
       </div>
       <div>
